4168e7d06cc543b9aacaa75021631041e079de05
Matthias Eilers <matthias.eilers@kuestenschmiede.de>
2017-08-24 14:16:17 +0200

    replace copyright

--- Resources/contao/modules/api/C4gForumAjaxApi.php
+++ Resources/contao/modules/api/C4gForumAjaxApi.php
@@ -3,7 +3,7 @@
 /**
  * con4gis - the gis-kit
  *
- * @version   php 5
+ * @version   php 7
  * @package   con4gis
  * @author    con4gis contributors (see "authors.txt")
  * @license   GNU/LGPL http://opensource.org/licenses/lgpl-3.0.html
