44888242612b1600acc6df822bd1468052b23dd7
Matthias Eilers <matthias.eilers@kuestenschmiede.de>
2018-02-21 17:38:03 +0100

    copyright 2018

--- Resources/contao/classes/C4GForumHelper.php
+++ Resources/contao/classes/C4GForumHelper.php
@@ -8,7 +8,7 @@
  * @package   con4gis
  * @author    con4gis contributors (see "authors.txt")
  * @license   GNU/LGPL http://opensource.org/licenses/lgpl-3.0.html
- * @copyright Küstenschmiede GmbH Software & Design 2011 - 2017.
+ * @copyright Küstenschmiede GmbH Software & Design 2011 - 2018
  * @link      https://www.kuestenschmiede.de
  */
 
@@ -23,7 +23,7 @@ use Contao\System;
 /**
  * Class C4GForumHelper
  *
- * @copyright  Küstenschmiede GmbH Software & Design 2012
+ * @copyright  Küstenschmiede GmbH Software & Design 2011 - 2018
  * @author     Jürgen Witte <http://www.kuestenschmiede.de>
  * @package    con4gis
  * @author     Jürgen Witte <http://www.kuestenschmiede.de>
