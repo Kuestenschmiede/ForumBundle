87ac214009281f7db2ee3ce7dcc515dfcbcff61b
coastforge-rro <raphael.ross@kuestenschmiede.de>
2018-11-15 15:29:21 +0100

    Add models classes

---
+++ Resources/contao/models/C4GForumSubscriptionModel.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rro
+ * Date: 15.11.18
+ * Time: 14:59
+ */
+
+namespace con4gis\ForumBundle\Resources\contao\models;
+
+
+class C4GForumSubscriptionModel extends \Contao\Model
+{
+    protected static $strTable = 'tl_c4g_forum_subforum_subscription';
+}
