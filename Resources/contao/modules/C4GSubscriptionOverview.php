21201791572f54c69a11c1799d0347e9272de151
coastforge-rro <raphael.ross@kuestenschmiede.de>
2018-11-15 13:03:41 +0100

    Add subscription module class and template file

---
+++ Resources/contao/modules/C4GSubscriptionOverview.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * con4gis - the gis-kit
+ *
+ * @version   php 5
+ * @package   con4gis
+ * @author    con4gis contributors (see "authors.txt")
+ * @license   GNU/LGPL http://opensource.org/licenses/lgpl-3.0.html
+ * @copyright Küstenschmiede GmbH Software & Design 2011 - 2018
+ * @link      https://www.kuestenschmiede.de
+ */
+
+namespace con4gis\ForumBundle\Resources\contao\modules;
+
+/**
+ * Class C4GSubscriptionOverview
+ * @package con4gis\ForumBundle\Resources\contao\modules
+ */
+class C4GSubscriptionOverview extends \Module
+{
+    protected $strTemplate = 'mod_c4g_forum_subscription_overview';
+
+    protected function compile()
+    {
+
+    }
+
+
+}
