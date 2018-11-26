7983bfd6eff89fde8f182db3d93401c55d15c324
coastforge-rro <raphael.ross@kuestenschmiede.de>
2018-11-26 11:55:43 +0100

    add a language file

---
+++ Resources/contao/languages/de/subscriptions.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * con4gis - the gis-kit
+ *
+ * @version   php 7
+ * @package   con4gis
+ * @author    con4gis contributors (see "authors.txt")
+ * @license   GNU/LGPL http://opensource.org/licenses/lgpl-3.0.html
+ * @copyright Küstenschmiede GmbH Software & Design 2011 - 2018
+ * @link      https://www.kuestenschmiede.de
+ */
+
+$GLOBALS['TL_LANG']['C4G_FORUM_SUBS']['SUCCESS_TITLE'] = 'Erfolg';
+$GLOBALS['TL_LANG']['C4G_FORUM_SUBS']['SUCCESS_MESSAGE'] = 'Das Abonnement wurde erfolgreich angepasst.';
+$GLOBALS['TL_LANG']['C4G_FORUM_SUBS']['SUCCESS_TITLE_DELETE'] = 'Erfolg';
+$GLOBALS['TL_LANG']['C4G_FORUM_SUBS']['SUCCESS_MESSAGE_DELETE'] = 'Das Abonnement wurde erfolgreich gelöscht.';
+$GLOBALS['TL_LANG']['C4G_FORUM_SUBS']['ERROR_TITLE'] = 'Fehler';
+$GLOBALS['TL_LANG']['C4G_FORUM_SUBS']['ERROR_MESSAGE'] = 'Ein Fehler ist aufgetreten.';
