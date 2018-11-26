1fdbfe238ad48bb98d4a019cbaea7bc268e38a2c
coastforge-rro <raphael.ross@kuestenschmiede.de>
2018-11-26 11:56:09 +0100

    fix copyright hints

--- Controller/SubscriptionController.php
+++ Controller/SubscriptionController.php
@@ -1,9 +1,13 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: cro
- * Date: 15.09.17
- * Time: 15:46
+ * con4gis - the gis-kit
+ *
+ * @version   php 7
+ * @package   con4gis
+ * @author    con4gis contributors (see "authors.txt")
+ * @license   GNU/LGPL http://opensource.org/licenses/lgpl-3.0.html
+ * @copyright Küstenschmiede GmbH Software & Design 2011 - 2018
+ * @link      https://www.kuestenschmiede.de
  */
 
 namespace con4gis\ForumBundle\Controller;
