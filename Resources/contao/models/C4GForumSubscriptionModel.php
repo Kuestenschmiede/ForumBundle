ac0fdd6638ca7f81613f0b6cac8c95e25dd83ec1
coastforge-rro <raphael.ross@kuestenschmiede.de>
2018-12-19 14:25:02 +0100

    add findby methods

--- Resources/contao/models/C4GForumSubscriptionModel.php
+++ Resources/contao/models/C4GForumSubscriptionModel.php
@@ -9,7 +9,19 @@
 namespace con4gis\ForumBundle\Resources\contao\models;
 
 
+use Contao\Database;
+
 class C4GForumSubscriptionModel extends \Contao\Model
 {
     protected static $strTable = 'tl_c4g_forum_subforum_subscription';
+
+    public function findByForumAndMember($forumId, $memberId) {
+        return new self(
+            Database::getInstance()->prepare(
+                "SELECT * FROM ".
+                self::$strTable.
+                " WHERE pid = ? AND member = ?")
+                ->execute($forumId, $memberId)
+        );
+    }
 }
--- Resources/contao/models/C4GThreadSubscriptionModel.php
+++ Resources/contao/models/C4GThreadSubscriptionModel.php
@@ -9,7 +9,19 @@
 namespace con4gis\ForumBundle\Resources\contao\models;
 
 
+use Contao\Database;
+
 class C4GThreadSubscriptionModel extends \Contao\Model
 {
     protected static $strTable = 'tl_c4g_forum_thread_subscription';
+
+    public function findByThreadAndMember($threadId, $memberId) {
+        return new self(
+            Database::getInstance()->prepare(
+                "SELECT * FROM ".
+                self::$strTable.
+                " WHERE pid = ? AND member = ?")
+                ->execute($threadId, $memberId)
+        );
+    }
 }
