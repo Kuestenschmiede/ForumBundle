bf3cd8c85c8326964b63b816fee2167e7c80e658
coastforge-rro <raphael.ross@kuestenschmiede.gmbh>
2021-04-06 13:19:55 +0200

    Add tl_c4g_forum_reaction and tl_c4g_forum_post_reaction

---
+++ Resources/contao/dca/tl_c4g_forum_post_reaction.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * This file is part of con4gis,
+ * the gis-kit for Contao CMS.
+ *
+ * @package con4gis
+ * @version 7
+ * @author  con4gis contributors (see "authors.txt")
+ * @license LGPL-3.0-or-later
+ * @copyright   Küstenschmiede GmbH Software & Design
+ * @link    https://www.con4gis.org
+ *
+ */
+
+$GLOBALS['TL_DCA']['tl_c4g_forum_post_reaction'] = [
+    'config' => [
+        'sql' => [
+            'keys' => [
+                'id' => 'primary'
+            ],
+        ],
+    ],
+
+    'fields' => [
+        'id' => [
+            'sql' => 'int unsigned NOT NULL auto_increment',
+        ],
+        'postId' => [
+            'sql' => 'int NOT NULL default 0',
+        ],
+        'memberId' => [
+            'sql' => 'int NOT NULL default 0',
+        ],
+        'reactionId' => [
+            'sql' => 'int NOT NULL default 0',
+        ]
+    ]
+];
